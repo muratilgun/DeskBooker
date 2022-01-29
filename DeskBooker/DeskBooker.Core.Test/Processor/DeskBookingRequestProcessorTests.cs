d56610a697ef1ec0748ac8e40536311a4584830f
muratilgun <muratilgun34@gmail.com>
2022-01-29 14:16:03 +0300

    Implemented first test
    New classes and methods were needed to implement the requirement. Therefore;
    - DeskBookingRequestProcessor class
    - BookDesk method
    - Request Class
    - Result class
    were written.

--- DeskBooker/DeskBooker.Core.Test/Processor/DeskBookingRequestProcessorTests.cs
+++ DeskBooker/DeskBooker.Core.Test/Processor/DeskBookingRequestProcessorTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace DeskBooker.Core.Processor
 {
@@ -7,7 +8,24 @@ namespace DeskBooker.Core.Processor
         [Fact]
         public void ShouldReturnDeskBookingResultWithRequestValues()
         {
+            //Arrenge
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Murat",
+                LastName = "İlgün",
+                Email = "muratilgun34@gmail.com",
+                Date = DateTime.Now
+            };
+            var processor = new DeskBookingRequestProcessor();
+            // Act
+            DeskBookingResult result = processor.BookDesk(request);
 
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName,result.FirstName);
+            Assert.Equal(request.LastName,result.LastName);
+            Assert.Equal(request.Email,result.Email);
+            Assert.Equal(request.Date,result.Date);
         }
     }
 }
