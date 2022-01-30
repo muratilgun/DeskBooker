294e2e1b336cc86d4bbc405bbfba56371f8c1ee7
muratilgun <muratilgun34@gmail.com>
2022-01-30 14:40:27 +0300

    ShouldSaveDeskBooking Green Test

--- DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -6,8 +6,11 @@ namespace DeskBooker.Core.Processor
 {
     public class DeskBookingRequestProcessor
     {
+        private readonly IDeskBookingRepository _deskBookingRepository;
+
         public DeskBookingRequestProcessor(IDeskBookingRepository deskBookingRepository)
         {
+            _deskBookingRepository = deskBookingRepository;
         }
 
         public DeskBookingResult BookDesk(DeskBookingRequest request)
@@ -16,6 +19,13 @@ namespace DeskBooker.Core.Processor
             {
                 throw new ArgumentNullException(nameof(request));
             }
+            _deskBookingRepository.Save(new DeskBooking
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date,
+            });
             return new DeskBookingResult
             {
                 FirstName = request.FirstName,
